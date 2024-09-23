97f4378d55d015f3c7de95379ccd6310d73084d5
Gokac9 <theabraham9@gmail.com>
2024-09-23 11:36:28 +0200

    initial push for the module

---
+++ control_de_transporte/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Control de Transporte Sale Order",
+    "summary": "Extensión del pedido de venta",
+    "version": "17.0.1.0.0",
+    "description": "Extensión del pedido de venta Carrascosa",
+    "company": "Xtendoo",
+    "website": "http://www.xtendoo.es",
+    "category": "Sales",
+    "depends": ["sale"],
+    "license": "AGPL-3",
+    "data": [
+        "views/sale_order.xml",
+    ],
+    "installable": True,
+}
---
+++ control_de_transporte/models/sale_order.py
@@ -0,0 +1,76 @@
+from odoo import api, models, fields
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    empresa_cargadora = fields.Many2one(
+        'res.partner',
+        string='Empresa Cargadora',
+        domain=[('is_company', '=', True)]
+    )
+    conductor = fields.Many2one(
+        'res.partner',
+        string='Conductor',
+        domain=[('is_company', '=', False)]
+    )
+    matricula_vehiculo = fields.Char(
+        string='Matrícula Vehículo'
+    )
+    matricula_semirremolque = fields.Char(
+        string='Matrícula Semirremolque'
+    )
+    empresa_expedidora = fields.Many2one(
+        'res.partner',
+        string='Empresa Expedidora',
+        domain=[('is_company', '=', True)]
+    )
+    operador_transporte = fields.Many2one(
+        'res.partner',
+        string='Operador de transporte',
+        domain=[('is_company', '=', False)]
+    )
+    empresa_destinataria = fields.Many2one(
+        'res.partner',
+        string='Empresa Destinataria',
+        domain=[('is_company', '=', True)]
+    )
+    reservas_y_observaciones = fields.Char(
+        string="Reservas y observaciones"
+    )
+    lugar_carga = fields.Char(
+        string='Lugar de Carga'
+    )
+    hora_llegada_carga = fields.Datetime(
+        string='Hora de Llegada'
+    )
+    hora_salida_carga = fields.Datetime(
+        string='Hora de Salida'
+    )
+    lugar_entrega= fields.Char(
+        string='Lugar de Entrega'
+    )
+    hora_llegada_entrega = fields.Datetime(
+        string='Hora de Llegada'
+    )
+    hora_salida_entrega = fields.Datetime(
+        string='Hora de Salida'
+    )
+    descripcion_mercancia = fields.Char(
+        string="Descripción"
+    )
+    peso = fields.Integer(
+        string="Peso aproximado"
+    )
+    km_llegada = fields.Float(
+        string="Km. LLegada"
+    )
+    km_salida = fields.Float(
+        string="Km. Salida"
+    )
+    km_total = fields.Float(string="Total km", compute='_compute_km_total', store=True)
+
+    @api.depends('km_llegada', 'km_salida')
+    def _compute_km_total(self):
+        for order in self:
+            order.km_total = order.km_llegada + order.km_salida
+
